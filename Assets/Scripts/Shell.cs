66ffa3524fae700c20dd5850e679699b5b5a2162
Dave Briccetti <daveb@davebsoft.com>
2020-08-01 13:34:41 -0700

    Simplify shell code

--- Assets/Scripts/Shell.cs
+++ Assets/Scripts/Shell.cs
@@ -1,25 +1,16 @@
 ﻿using UnityEngine;
 
 public class Shell : MonoBehaviour {
-    private float firedAtTime;
     public Transform hitEffectPrefab;
 
     private void Start() {
-        firedAtTime = Time.time;
-    }
-
-    private void Update() {
-        if (Time.time > firedAtTime + 5) {
-            Destroy(gameObject);
-        }
+        Destroy(gameObject, 5);
     }
 
     private void OnCollisionEnter(Collision collision) {
         if (!collision.gameObject.CompareTag("Enemy")) return;
-        var he = Instantiate(hitEffectPrefab, collision.transform.position, Quaternion.identity);
-        var ps = he.GetComponent<ParticleSystem>();
         var directionBackTowardGun = transform.position - collision.gameObject.transform.position;
-        ps.transform.rotation = Quaternion.LookRotation(directionBackTowardGun);
+        Instantiate(hitEffectPrefab, collision.transform.position, Quaternion.LookRotation(directionBackTowardGun));
         Destroy(gameObject);
     }
 }
