d61c9e143b97a31c99f097aaf3629dec0b907263
Dave Briccetti <daveb@davebsoft.com>
2020-07-31 17:56:24 -0700

    Use a coroutine to turn off the muzzle flash

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -1,9 +1,10 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
 public class Gun : MonoBehaviour {
     public Transform shellPrefab;
     public Transform muzzleFlashPrefab;
-    private float fireDelay = 0.5f;
+    public float fireDelay = 0.5f;
     public int range = 3;
     public EnemyManager EnemyManager { get; set; }
 
@@ -11,13 +12,12 @@ public class Gun : MonoBehaviour {
     private Transform firePoint;
     private Transform flashPoint;
     private float nextFireTime;
-    private float flashOffTime;
     private Light flash;
     private Transform muzzleFlash;
+    private const float FlashDuration = 0.12f;
 
     void Start() {
         nextFireTime = Time.time + Random.Range(0f, 1f);
-        flashOffTime = Time.time;
         gunBody = transform.Find("Body");
         firePoint = gunBody.Find("Barrel/Fire Point");
         flashPoint = gunBody.Find("Barrel/Flash Point");
@@ -27,8 +27,6 @@ public class Gun : MonoBehaviour {
 
     void Update() {
         if (!EnemyManager) return;
-        if (flash.enabled && Time.time > flashOffTime)
-            flash.enabled = false;
         var closest = EnemyManager.ClosestEnemyTo(transform.position, range);
         if (closest != null) {
             Vector3 aimAt = closest.position + closest.forward * .1f; // A little bit ahead
@@ -43,11 +41,16 @@ public class Gun : MonoBehaviour {
         if (Time.time < nextFireTime) return;
         muzzleFlash.position = flashPoint.position;
         flash.enabled = true;
-        flashOffTime = Time.time + 0.12f;
+        StartCoroutine("TurnOffFlash");
         var shellRotation = gunBody.rotation;
         var shell = Instantiate(shellPrefab, firePoint.position, shellRotation);
         shell.Rotate(Vector3.right, 90);
         shell.GetComponent<Rigidbody>().AddForce(gunBody.forward * 1000f);
         nextFireTime = Time.time + fireDelay + Random.Range(0, .1f);
+    }
+
+    IEnumerator TurnOffFlash() {
+        yield return new WaitForSeconds(FlashDuration);
+        flash.enabled = false;
     }
 }
