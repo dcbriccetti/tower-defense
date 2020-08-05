7648a960bbe83a31814f0bc4710f5dbc35b30986
Dave Briccetti <daveb@davebsoft.com>
2020-08-05 11:43:48 -0700

    Fix bug causing gun to fire at the wrong times

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -13,6 +13,7 @@ public class Gun : MonoBehaviour {
     private Transform firePoint;
     private float nextFireTime;
     private Light flash;
+    private AudioSource audioSource;
     private const float FlashDuration = 0.05f;
 
     private void Start() {
@@ -20,28 +21,25 @@ public class Gun : MonoBehaviour {
         gunBody = transform.Find("Body");
         firePoint = gunBody.Find("Barrel/Fire Point");
         flash = gunBody.Find("Barrel/Muzzle Flash").GetComponent<Light>();
+        audioSource = firePoint.GetComponent<AudioSource>();
     }
 
     private void Update() {
         var closest = EnemyManager.Instance.ClosestEnemyTo(transform.position, range);
         if (closest == null) return;
 
-        Quaternion RotationToTarget() {
-            Vector3 directionToTarget = closest.position - gunBody.position;
-            return Quaternion.LookRotation(directionToTarget);
-        }
-
+        Quaternion RotationToTarget() => Quaternion.LookRotation(closest.position - gunBody.position);
         Vector3 rotation = Quaternion.Lerp(gunBody.rotation, RotationToTarget(), Time.deltaTime * rotationSpeed).eulerAngles;
         gunBody.rotation = Quaternion.Euler(0f, rotation.y, 0f);
         
-        if (Time.time < nextFireTime && Mathf.Abs(RotationToTarget().y) < 1f) {
-            FireWhenReady();
+        if (Time.time > nextFireTime && Mathf.Abs(RotationToTarget().y) < .1f) {
+            Fire();
             nextFireTime = Time.time + fireDelay + Random.Range(-.1f, .1f);
         }
     }
 
-    private void FireWhenReady() {
-        firePoint.GetComponent<AudioSource>().Play();
+    private void Fire() {
+        audioSource.Play();
         flash.enabled = true;
         StartCoroutine(nameof(TurnOffFlash));
         var shellRotation = gunBody.rotation;
