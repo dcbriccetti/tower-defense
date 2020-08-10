8af0986ccccefae8c5b70d3e4a2fa5f72bd5e92d
Dave Briccetti <daveb@davebsoft.com>
2020-08-10 12:40:50 -0700

    Add camera view up/down arrow item positioning

--- Assets/Scripts/CameraPositioner.cs
+++ Assets/Scripts/CameraPositioner.cs
@@ -1,43 +1,51 @@
+using System;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UIElements;
 
 public class CameraPositioner : MonoBehaviour {
     [SerializeField] [Range(1, 30)] private int positionChangeSpeed = 3;
     [SerializeField] [Range(1, 30)] private int turnSpeed = 3;
     private new Transform camera;
+    private readonly CameraView[] cameraViews = {
+        new NormalView(),
+        new FollowItemCameraView("Weapon"),
+        new FollowItemCameraView("Enemy")
+    };
+    private CameraView cameraView;
+    private CameraState cameraState;
+    private int iCameraView;
 
-    /// <summary>
-    /// Advances to the next enabled view
-    /// </summary>
+    /// Advances to the next enabled view 
     public void ChangeView() {
         var activeViewFound = false;
         while (! activeViewFound) {
             iCameraView = (iCameraView + 1) % cameraViews.Length;
             if (cameraViews[iCameraView].IsActive()) activeViewFound = true;
         }
+
+        cameraView = cameraViews[iCameraView];
     }
     
-    /// <summary>
     /// Returns whether the current view is the normal one, i.e., the view from above
-    /// </summary>
-    /// <returns>Whether the current view is the normal one</returns>
-    public bool IsNormalView() => cameraViews[iCameraView].GetType() == typeof(NormalView);
-
-    private readonly CameraView[] cameraViews = {
-        new NormalView(),
-        new FollowItemCameraView("Weapon", 0),
-        new FollowItemCameraView("Enemy", 1)
-    };
-
-    private CameraState cameraState;
-    private int iCameraView;
+    public bool IsNormalView() => cameraView.GetType() == typeof(NormalView);
 
     private void Start() {
+        cameraView = cameraViews[0];
         camera = Camera.main.transform;
         cameraState = new CameraState(camera.position, camera.rotation);
     }
 
+    private void Update() {
+        var kb = Keyboard.current;
+        if (kb.upArrowKey.wasPressedThisFrame)
+            cameraView.ProcessUpDown(1);
+        else if (kb.downArrowKey.wasPressedThisFrame)
+            cameraView.ProcessUpDown(-1);
+    }
+
     private void LateUpdate() {
-        cameraViews[iCameraView].SetCamera(cameraState);
+        cameraView.SetCamera(cameraState);
         SmoothlyMoveAndTurnCamera();
     }
 
--- Assets/Scripts/CameraView.cs
+++ Assets/Scripts/CameraView.cs
@@ -1,57 +1,57 @@
 using System;
 using UnityEngine;
 
-/// <summary>
 /// Base class for camera views
-/// </summary>
 internal abstract class CameraView  {
     /// <summary>
     /// Sets the camera’s position and rotation
     /// </summary>
     /// <param name="cameraState">The camera’s state</param>
     protected internal abstract void SetCamera(CameraState cameraState);
-    /// <summary>
+
     /// Returns whether the view is active. If there are no guns, a guns view will be inactive.
-    /// </summary>
-    /// <returns>Whether the view is active</returns>
     public abstract bool IsActive();
+
+    public virtual void ProcessMouseWheelInput(float displacement) { }
+
+    protected internal virtual void ProcessUpDown(int upDown) { }
+
+    public virtual void ProcessLeftRight(float leftRight) { }
 }
 
-/// <summary>
 /// A camera view that follows one of a set of tagged items (enemies, guns)
-/// </summary>
 internal class FollowItemCameraView : CameraView {
     private readonly string tag;
-    private readonly int itemIndex;
+    private int itemIndex;
 
     /// <summary>
     /// Creates a camera view that can follow a moving enemy in a group of enemies,
     /// or a still object such as a gun
     /// </summary>
     /// <param name="tag">The tag of an item or group of items</param>
-    /// <param name="itemIndex">The index of the item in a group to follow</param>
-    public FollowItemCameraView(string tag, int itemIndex) {
+    public FollowItemCameraView( string tag) {
         this.tag = tag;
-        this.itemIndex = itemIndex;
     }
 
     protected internal override void SetCamera(CameraState cameraState) {
         GameObject[] items = GetItems();
         if (items.Length < 1) return;
-        var selectedItemTransform = items[Math.Min(items.Length - 1, itemIndex)].transform;
-        var behindAndAbove = -selectedItemTransform.forward + Vector3.up / 2;
-        cameraState.desiredPosition = selectedItemTransform.position + behindAndAbove;
-        cameraState.desiredRotation = selectedItemTransform.rotation;
+        var item = items[Math.Min(items.Length - 1, itemIndex)].transform;
+        var behindAndAbove = -item.forward + Vector3.up / 2;
+        cameraState.desiredPosition = item.position + behindAndAbove;
+        cameraState.desiredRotation = item.rotation;
     }
 
+    protected internal override void ProcessUpDown(int upDown) {
+        itemIndex = Math.Max(0, itemIndex - upDown); // Up: closer to first element, Down: closer to last
+    }
+    
     private GameObject[] GetItems() => GameObject.FindGameObjectsWithTag(tag);
 
     public override bool IsActive() => GetItems().Length > 0;
 }
 
-/// <summary>
 /// A normal view, centered on the scene from above
-/// </summary>
 internal class NormalView : CameraView {
     protected internal override void SetCamera(CameraState cameraState) {
         cameraState.desiredPosition = cameraState.normalPosition;
